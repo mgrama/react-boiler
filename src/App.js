6f26b5ebcf9e9e6fbd2a3176ec5a42900f02ad37
Mark Grama <mark@grama.ru>
2018-06-08 14:25:11 +0300

    data to tables

--- src/App.js
+++ src/App.js
@@ -2,56 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 import { arr } from './example.js'
 
-const TABLE_COLUMNS = [
-  {
-    label: 'Имя',
-    sort: 'default',
-  },
-  {
-    label: 'Век',
-    sort: 'default',
-  },
-  {
-    label: 'Активность',
-    sort: 'default',
-  },
-];
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data: [],
-      columns: TABLE_COLUMNS
-    }
-  }
-
-  componentDidMount() {
-    const count = this.count;
-    const ids = arr.map(function(id) {
-      return id.id
-    })
-    console.log(ids)
-    const objs = arr.reduce((sum, i) => ({ ...sum, [i.id]: i }), {});
-    console.log(objs)
-  }
-
-  renderOptions() {
-    const objs = this.state
-  }
 
   render() {
     return (
       <div className="App">
-        {this.renderOptions()}
+        <table>
+          <tr>
+            <td>
+              <ol>
+                {arr.map((item) =>
+                <li key={item.id}>
+                  <td>
+                    <h3>{item.name}</h3>
+                  </td>
+                  <td>
+                    <p>{item.century}</p>
+                  </td>
+                  <td>
+                    <p>{item.ekzeget_type.title}</p>
+                  </td>
+                </li>
+                )}
+              </ol>
+            </td>
+          </tr>
+
+        </table>
+
       </div>
     );
   }
 }
-
-
-
-
 export default App;
