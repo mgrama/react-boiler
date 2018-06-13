4d1dd3c5373618b38c6e6bc8c85a56b6b6af4294
Mark Grama <mark@grama.ru>
2018-06-13 11:22:19 +0300

    added propTypes

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
-import { arr } from './example.js'
-
+import { arr } from './example.js';
+import PropTypes from 'prop-types';
 
 class App extends Component {
 
   render() {
+    let name = this.props.name
     return (
       <div className="App">
-                {arr.map((item, i) =>
-                  <li key={item.i}>
-                    <div>
-                      <p>{i}</p>
-                    </div>
-                    <div>
-                      <h3>{item.name}</h3>
-                    </div>
-                    <div>
-                      <p>{item.century}</p>
-                    </div>
-                    <div>
-                      <p>{item.ekzeget_type.title}</p>
-                    </div>
-                  </li>
-                )}
-
+        <ol>
+          {arr.map((item, i) =>
+            <li key={item.i}>  <div>
+                    <p>{i}</p>
+                  </div>
+                  <div>
+                    <h3>{item.name}</h3>
+                  </div>
+                  <div>
+                    <p>{item.century}</p>
+                  </div>
+                  <div>
+                    <p>{item.ekzeget_type.title}</p>
+                  </div>
+                </li>
+              )}
+        </ol>
       </div>
-    );
+
+    )
+
+    App.propTypes = {
+      name: PropTypes.string.isRequired,
+      century: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired
+    }
   }
+
+
 }
+
 export default App;
